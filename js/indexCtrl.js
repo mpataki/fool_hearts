d1d5cb83d7d7dda89d1ac4d9a24a0d44c7b7f4fd
mpataki <matpataki@gmail.com>
2014-08-26 10:34:08 -0400

    social link images in

--- js/indexCtrl.js
+++ js/indexCtrl.js
@@ -2,7 +2,8 @@ var indexCtrl = angular.module('indexCtrl', ['duParallax']);
 
 indexCtrl.controller('indexCtrl', function($scope, parallaxHelper) {
 
-	$scope.background = parallaxHelper.createAnimator(-0.7);
+	$scope.background = parallaxHelper.createAnimator(-0.75);
+	$scope.title = parallaxHelper.createAnimator(-0.3);
 
 	console.log("indexCtrl loaded");
 });
