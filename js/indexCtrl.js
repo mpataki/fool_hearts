874d39fe4ce141c6a11405963811955cd7ccd8cb
mpataki <matpataki@gmail.com>
2014-08-27 16:08:25 -0400

    fixed nav selection bug

--- js/indexCtrl.js
+++ js/indexCtrl.js
@@ -1,8 +1,29 @@
-var indexCtrl = angular.module('indexCtrl', ['duParallax']);
+var indexCtrl = angular.module('indexCtrl', ['ngRoute', 'duParallax']);
 
-indexCtrl.controller('indexCtrl', function($scope, parallaxHelper) {
+indexCtrl.controller('indexCtrl', function($scope, $location, parallaxHelper) {
 
-	$scope.navSelection = 0;
+	switch($location.$$path){
+		case '/home':
+			$scope.navSelection = 0;
+			break;
+		case '/media':
+			$scope.navSelection = 1;
+			break;
+		case '/repertoire':
+			$scope.navSelection = 2;
+			break;
+		case '/members':
+			$scope.navSelection = 3;
+			break;
+		case '/weddings':
+			$scope.navSelection = 4;
+			break;
+		case '/contact_quote':
+			$scope.navSelection = 5;
+			break;
+		default:
+			$scope.navSelection = 0;
+	}
 
 	$scope.background = parallaxHelper.createAnimator(-0.75);
 	$scope.title = parallaxHelper.createAnimator(-0.3);
