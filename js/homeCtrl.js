9fd92a0eb38b074b99561907ab93503276b16922
mpataki <matpataki@gmail.com>
2014-08-28 18:28:44 -0400

    home entry transitions

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,5 +1,9 @@
 var homeCtrl = angular.module('homeCtrl', []);
 
 homeCtrl.controller("homeCtrl", function($scope) {
+
+	$scope.showCarousel = false;
+	$scope.showBlurb = false;
+
 	console.log("homeCtrl loaded");
 });
