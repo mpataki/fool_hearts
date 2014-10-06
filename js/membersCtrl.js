d9b1d334f5eecedd28babec41952949dd5c22010
mpataki <matpataki@gmail.com>
2014-10-06 17:23:33 -0400

    members fade in

--- js/membersCtrl.js
+++ js/membersCtrl.js
@@ -1,5 +1,12 @@
 var membersCtrl = angular.module('membersCtrl', []);
 
 membersCtrl.controller("membersCtrl", function($scope) {
+
+	$scope.membersInView = [false, false, false, false, false, false, false, false];
+
+	$scope.$on('navigateAway', function(){
+		$scope.membersInView = [false, false, false, false, false, false, false, false];
+	});
+
 	console.log("membersCtrl loaded");
 });
