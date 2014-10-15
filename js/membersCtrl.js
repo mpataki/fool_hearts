32ea4f5c24278a19b3a10234d30fbfd1ea77f0a0
mpataki <matpataki@gmail.com>
2014-10-15 01:10:19 -0400

    new testimonials and more members!

--- js/membersCtrl.js
+++ js/membersCtrl.js
@@ -2,10 +2,10 @@ var membersCtrl = angular.module('membersCtrl', []);
 
 membersCtrl.controller("membersCtrl", function($scope) {
 
-	$scope.membersInView = [false, false, false, false, false, false, false, false];
+	$scope.membersInView = [false, false, false, false, false, false, false, false, false, false];
 
 	$scope.$on('navigateAway', function(){
-		$scope.membersInView = [false, false, false, false, false, false, false, false];
+		$scope.membersInView = [false, false, false, false, false, false, false, false, false, false];
 	});
 
 	console.log("membersCtrl loaded");
