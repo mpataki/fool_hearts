5456aaa4624a8a052d2797a6d7472eaffaef977c
mpataki <matpataki@gmail.com>
2015-04-05 22:57:37 -0400

    remove wedding page in-view

--- js/weddingsCtrl.js
+++ js/weddingsCtrl.js
@@ -2,14 +2,12 @@ var weddingsCtrl = angular.module('weddingsCtrl', ['angular-inview']);
 
 weddingsCtrl.controller("weddingsCtrl", function($scope) {
 
-	$scope.CheckboxItemInView = [false, false, false, false, false, false, false];
 	$scope.weddingBlurbIn = false;
 	$scope.checklistIn = false;
 
 	$scope.$on('navigateAway', function(){
 		$scope.weddingBlurbIn = false;
 		$scope.checklistIn = false;
-		$scope.CheckboxItemInView = [false, false, false, false, false, false, false];
 	});
 
 	console.log("weddingsCtrl loaded");
