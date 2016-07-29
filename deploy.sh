85e87bc13f1ad2afb07ca203e73ff04af7eb6efa
Mat Pataki <matpataki@gmail.com>
2016-07-29 16:50:45 -0400

    add region to aws cli command

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Starting deploy"
-aws s3 sync . s3://foolhearts.com/ --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
+aws s3 sync . s3://foolhearts.com/ --region "us-west-2" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
 exit $?
