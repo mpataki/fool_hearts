0496e5e978b2df7295d18bf98408c77fcad28f66
Mat Pataki <matpataki@gmail.com>
2016-07-29 16:43:08 -0400

    add logging

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+echo "Starting deploy"
 aws s3 sync . s3://foolhearts.com/ --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
+echo "Deploy finished"
