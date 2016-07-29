3e5ab94e28632d0eec9cdf40507ee3df8136f18a
Mat Pataki <matpataki@gmail.com>
2016-07-29 16:47:44 -0400

    make sure the script fails if the deploy fails

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 echo "Starting deploy"
 aws s3 sync . s3://foolhearts.com/ --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
-echo "Deploy finished"
+exit $?
