9c165657cf7c3e21fe7ba960dedc8182330257fd
Mat Pataki <matpataki@gmail.com>
2017-10-15 14:56:40 -0400

    cirlce deploy for staging and prod

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SITE=$1
+
 echo "Starting deploy"
-aws s3 sync . s3://foolhearts.com/ --region "us-west-2" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
+aws s3 sync . s3://$SITE --region "us-west-2" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
 exit $?
