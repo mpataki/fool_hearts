2bac3daa31c78872f87b4fb8a7f799417a3f5a7a
Mat Pataki <matpataki@gmail.com>
2017-10-15 16:42:08 -0400

    fix the region for real

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 SITE=$1
 
 echo "Starting deploy"
-aws s3 sync . s3://$SITE --region "us-west-1" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
+aws s3 sync . s3://$SITE --region "us-east-1" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
 exit $?
