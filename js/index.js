a2aa817f01dfae4e10461a1006ac773d4e88cdb3
Mat Pataki <matpataki@gmail.com>
2017-12-01 16:06:31 -0800

    add rough testimonials

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
+slickOptions = {
+    lazyLoad: 'progressive',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    mobileFirst: true
+};
+
 $(document).ready(function() {
-    $('.videos').slick({
-        lazyLoad: 'progressive',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        mobileFirst: true
-    });
+    $('.videos').slick(slickOptions);
+    $('.testimonials').slick(slickOptions);
 });
