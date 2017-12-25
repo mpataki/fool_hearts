51ba8a6e6034996e258402e006aebc5a6c681b2d
Mat Pataki <matpataki@gmail.com>
2017-12-25 02:23:38 -0500

    use js to position the title

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-slickOptions = {
-};
-
 $(document).ready(function() {
+
+    function update() {
+        var headerElm = document.getElementById('header');
+        var headerBoundingRect = headerElm.getBoundingClientRect();
+
+        var titleElem = document.getElementsByClassName('title')[0];
+        var titleBoundingRect = titleElem.getBoundingClientRect();
+
+        console.log("title top = " + titleBoundingRect.top);
+        console.log("header top = " + headerBoundingRect.top);
+        console.log("headerBoundingRect.height + headerBoundingRect.top = " + (headerBoundingRect.height + headerBoundingRect.top));
+
+        var top = (headerBoundingRect.height + headerBoundingRect.top) * 0.5 - titleBoundingRect.height / 2;
+        titleElem.style.top = top.toString() + "px";
+    }
+
     $('.videos').slick({
         draggable: true,
         mobileFirst: true,
@@ -16,4 +29,14 @@ $(document).ready(function() {
         slidesToScroll: 1,
         slidesToShow: 1
     });
+
+    window.scrollReveal = ScrollReveal();
+    window.scrollReveal.reveal('.video-section', {
+        duration: 600,
+        reset: true
+    });
+
+    $(window).scroll(update);
+
+    update(); // initialize
 });
