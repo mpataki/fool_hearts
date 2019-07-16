e73ccd6fb70a5ff39cb2a2e6372ec26669c1c474
Mat Pataki <matpataki@gmail.com>
2019-07-15 22:15:23 -0400

    remove deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SITE=$1
-
-echo "Starting deploy"
-aws s3 sync . s3://$SITE --region "us-east-1" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "bower.json" --exclude "README.md" --exclude "circle.yml"
-exit $?
